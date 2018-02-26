7a521ad408bcb7d16df64e56f68a2ead3517b21a
Xiong <xiokenw@dunwoody.edu>
2018-02-25 19:02:11 -0600

    Created program, added SuperBowl class

---
+++ super-bowl-project/super-bowl-project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace super_bowl_project
+{
+    class SuperBowlProject
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ super-bowl-project/super-bowl-project/SuperBowl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace super_bowl_project
+{
+    class SuperBowl
+    {
+        // Basic properties
+        public string Date { get; set; }
+        public string SuperBowlNum { get; set; }
+        public int Attendance { get; set; }
+
+        // Winners' properties =)
+        public string QBWinner { get; set; }
+        public string CoachWinner { get; set; }
+        public string Winner { get; set; }
+        public int WinningPoints { get; set; }
+        // Losers' properties =(
+        public string QBLoser { get; set; }
+        public string CoachLoser { get; set; }
+        public string Loser { get; set; }
+        public int LosingPoints { get; set; }
+        // Special Properties...not really though..kind of
+        public string MVP { get; set; }
+        public string Stadium { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
